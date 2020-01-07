446fe494cdce580b0a41c8fa354a93c0bd63eea3
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-01-07 11:48:00 -0600

    reset param

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -10,12 +10,17 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
     int hitCount = 0;
 
-
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         String name = req.getParameter("name");
         if (name == null || name.equals("")) {
             res.setContentType("text/html");
             hitCount++;
+            PrintWriter pw = res.getWriter();
+            pw.println("<h1>Hello, World!</h1>");
+            pw.println("<h1>Hitcount is " + hitCount + "!</h1>");
+        } else if (name.equalsIgnoreCase("resetcounter")){
+            res.setContentType("text/html");
+            hitCount = 0;
             PrintWriter pw = res.getWriter();
             pw.println("<h1>Hello, World!</h1>");
             pw.println("<h1>Hitcount is " + hitCount + "!</h1>");
